92a51a1119525bcdbbe7c811ee92e7cd0103528c
LazyLEE1008 <66935871+LazyLEE1008@users.noreply.github.com>
2023-10-12 08:58:24 +0900

    2667 단지붙히기
    어렵게 Union-find할 필요는 없었다!
    
    그래도 좋은 시도였다.
    
    dfs, bfs 두개다 가능하지만, 그래도 bfs가 내가 생각하기에 좀 편하다. 재귀함수는 아직 어렵다.

--- CodingTest/2667단지번호붙이기.cpp
+++ CodingTest/2667단지번호붙이기.cpp
@@ -1,24 +1,134 @@
 #include <iostream>
+#include <queue>
 #include <vector>
+#include <algorithm>
+
 
 using namespace std;
 
-const int MAX = 26;
+const int MAX = 25;
 
 int arr[MAX][MAX];
+bool visited[MAX][MAX];
+
 
 int main() {
 	
+	int N = 7;
+
+	vector<pair<int, int>> move_dir;
+	vector<int> v_cnt;
+	int cnt = 0;
+
+	//╩С ©Л го аб
+	move_dir.push_back(make_pair(1, 0));
+	move_dir.push_back(make_pair(0, 1));
+	move_dir.push_back(make_pair(-1, 0));
+	move_dir.push_back(make_pair(0, -1));
+
+
+	for (int i = 0; i < MAX; i++) {
+		for (int j = 0; j < MAX; j++) {
+
+			if ((i >= 0 && i < N) && (j >= 0 && j < N)) {
+				continue;
+			}
+
+			visited[i][j] = true;
+		}
+	}
+
+
+
 	string s = "0110100";
 
 	for (int i = 0; i < s.length(); i++) {
-		arr[1][i + 1] = s[i]-'0';
+		arr[0][i] = s[i]-'0';
 	}
 
-	for (int i = 1; i <= s.length(); i++) {
-		cout << arr[1][i];
+	s = "0110101";
+	for (int i = 0; i < s.length(); i++) {
+		arr[1][i] = s[i] - '0';
+	}
+
+	s = "1110101";
+	for (int i = 0; i < s.length(); i++) {
+		arr[2][i] = s[i] - '0';
+	}
+
+	s = "0000111";
+	for (int i = 0; i < s.length(); i++) {
+		arr[3][i] = s[i] - '0';
+	}
+
+	s = "0100000";
+	for (int i = 0; i < s.length(); i++) {
+		arr[4][i] = s[i] - '0';
+	}
+	s = "0111110";
+	for (int i = 0; i < s.length(); i++) {
+		arr[5][i] = s[i] - '0';
+	}
+	s = "0111000";
+	for (int i = 0; i < s.length(); i++) {
+		arr[6][i] = s[i] - '0';
 	}
 
 
+
+	queue<pair<int, int>> dir;
+
+	for (int k = 0; k < N; k++) {
+		for (int l = 0; l < N; l++) {
+
+			if (arr[k][l] == 0 || visited[k][l]) {
+				continue;
+			}
+
+			dir.push(make_pair(k, l));
+
+			visited[k][l] = true;
+
+			int next_y = 0;
+			int next_x = 0;
+
+
+			while (!dir.empty()) {
+				int cur_y = dir.front().first;
+				int cur_x = dir.front().second;
+
+				dir.pop();
+				cnt++;
+
+				for (int i = 0; i < move_dir.size(); i++) {
+					next_y = cur_y + move_dir[i].first;
+					next_x = cur_x + move_dir[i].second;
+
+					if ((0 <= next_y && next_y < MAX) && (0 <= next_x && next_x < MAX) && !visited[next_y][next_x] &&
+						(arr[next_y][next_x] == 1)) {
+
+						dir.push(make_pair(next_y, next_x));
+						visited[next_y][next_x] = true;
+
+					}
+
+				}
+
+			}
+
+			v_cnt.push_back(cnt);
+			cnt = 0;
+
+
+		}
+	}
+
+	sort(v_cnt.begin(), v_cnt.end());
+
+	
+	cout << v_cnt.size() << endl;
+	for (int i = 0; i < v_cnt.size(); i++) {
+		cout << v_cnt[i] << endl;
+	}
 
 }
